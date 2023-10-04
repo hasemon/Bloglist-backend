9ea8b3473bc56faf6c5a12f8c6fdc3d450c87f33
Shariar Hasan Emon <imemon.sh@gmail.com>
2023-10-04 09:56:41 +0600

    minor changes

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -4,9 +4,13 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test(' HTTP GET request to the "/api/blogs"  returns the correct amount of blog posts', async () => {
-    
-})
+test('HTTP GET request to the "/api/blogs"  returns the correct amount of blog posts', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect("Content-Type", /application\/json/)
+    expect(response.body.length).toEqual(3)
+
+}, 10000)
 
 
 
